187c5cb42d17ee1c377d2f9bfe04cfe22052b5d1
pedroeborges <pedro-borgescosta@hotmail.com>
2019-05-22 20:22:47 -0300

    att

--- respJS_003.js
+++ respJS_003.js
@@ -2,13 +2,7 @@ function Q1() {
     console.log('Q1');
     var aux = Number.parseInt(document.getElementById('numerosQ1').value);
     var pares = [];
-    //var parounao = (num) => {
-    //    if (num%2==0)
-    //    return true;
-    //    else
-    //    return false;
-    //};
-    for (let num = 0; num <= aux; num+=2) {
+    for (let num = 0; num <= aux; num += 2) {
         pares.push(num);
     }
     var resp = pares
@@ -19,23 +13,25 @@ function Q1() {
 function Q2() {
     console.log('Q2');
     var aux = Number.parseInt(document.getElementById('numerosQ2').value);
-    var aux2 = 0
     var primos = [];
-    var primoounao = (x) => {
-        for (num=2; num<x; num++) {
-            if (x%num==0)
-            return false;
+    var primoounao = x => {
+        for (let num = 2; num < x; num++) {
+            if (x % num == 0)
+                return false;
         }
-        if (primoounao!=false)
-        primoounao = true;
+        if (primoounao != false)
+            primoounao = true;
     }
-    for (let num=2; num<=aux; num++) {
-        if (primoounao(num)==true)
-        primos.push(num);
+    for (let num1 = 2; num1 <= aux; num1++) {
+        if (primoounao(num1) == true)
+            primos.push(num1);
     }
     var resp = primos
     document.getElementById('RQ2').innerHTML = "Texto da resposta: " + resp;
     console.log(resp)
-    }
+    console.log(primoounao(2))
+    console.log(primouounao(7))
+    console.log(primoounao(8))
+}
 
 
