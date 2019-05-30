b5d577df9636f809d69b2a02eca7e36c698331b4
pedroeborges <pedro-borgescosta@hotmail.com>
2019-05-30 07:09:04 -0300

    att

--- respJS_003.js
+++ respJS_003.js
@@ -29,16 +29,69 @@ function Q2() {
     console.log(primos);
 }
 
-function Q3() {
-    console.log('Q3'); 
-    var listaStrings = [];
-    var novaString = document.getElementById('novaString').value;
-    listaStrings.push(novaString);
-    for (let i = 0; i <= listaStrings.length; i++) {
-        listaStrings = listaStrings[i].toUpperCase();
-    }
-    document.getElementById('RQ3').innerHTML = "Texto da resposta: " + listaStrings;
-    console.log(listaStrings);
+var listaStrings = [];
+
+function acrescentaSTR() {
+	var aux = document.getElementById('novaString').value;
+	listaStrings.push(aux);
+	document.getElementById('novaString').value = "";
+	document.getElementById('auxQ3').innerHTML = listaStrings;
+	console.log(listaStrings);
 }
 
+function Q3() {
+    console.log('Q3');
+    for(let i = 0; i < listaStrings.length; i++) {
+		listaStrings[i] = listaStrings[i].toUpperCase();
+	}
+    console.log(listaStrings);
+    document.getElementById('RQ3').innerHTML = listaStrings;
+}
 
+function QuickSort_(A, lo, hi) { // X é array
+
+	function partition(A, lo, hi) {
+		var pivot = A[hi];
+		var i = lo;
+		var aux;
+		for(let j = lo; j < hi; j++) {
+			if(A[j] < pivot) {
+				aux = A[i];
+				A[i] = A[j];
+				A[j] = aux;
+				i++;
+			}
+		}
+		aux = A[i];
+		A[i] = A[hi];
+		A[hi] = aux;
+		return i;
+	}
+
+	var p;
+    if(lo < hi){
+        p = partition(A, lo, hi);
+        QuickSort_(A, lo, p - 1);
+        QuickSort_(A, p + 1, hi);
+    }
+}
+
+function QuickSort(X) {
+	var Y = Array.from(X);
+	QuickSort_(Y, 0, Y.length-1);
+	return Y;
+}
+
+function Q4() {
+    console.log('Q4');
+    var aux = document.getElementById('numerosQ4').value;
+    var numerosQ4 = aux.split(';');
+    var ordenados;
+    for (let i = 0; i < numerosQ4.length; i++) {
+		numerosQ4[i] = Number.parseFloat(numerosQ4[i]);
+	}
+	console.log('****' + numerosQ4);
+    ordenados = QuickSort(numerosQ4);
+    console.log(ordenados);
+    document.getElementById('RQ4').innerHTML = ordenados;
+}
