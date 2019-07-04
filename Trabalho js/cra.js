bbbb693d83257d006eb76e5b5dc445bb3b1c891f
pedroeborges <pedro-borgescosta@hotmail.com>
2019-07-03 21:16:15 -0300

    trabalho

--- Trabalho js/cra.js
+++ Trabalho js/cra.js
@@ -1,13 +1,21 @@
 function cra() {
-    var nota = [];
-    var cargahoraria = [];
-    var cargareprov = [];
+    var notas = "";
+    notas = document.getElementById('notas').value;
+    notas = notas.split(",").map(Number);
+    var cargahoraria = "";
+    cargahoraria = document.getElementById('horas').value;
+    cargahoraria = cargahoraria.split(",").map(Number);
+    var cargareprov = "";
+    cargareprov = document.getElementById('horasrep').value;
+    cargareprov = cargareprov.split(",").map(Number);
+    console.log(notas);
+    console.log(cargahoraria);
+    console.log(cargareprov);
     function multiplicalista(lst1, lst2) {
         // array array -> array
-        // multipica elemento i de uma lista pelo 
-        // correspondente em outra lista (sobrescreve na primeira array argumento)
-        var aux = lst1
-        for (i = 0; lst1[i] != null || lst2[i] != null; i++) {
+        // multipica elemento i de uma lista pelo correspondente em outra lista
+        var aux = [];
+        for (i = 0; i < (lst1.length) || i < (lst2.length); i++) {
             aux[i] = lst1[i] * lst2[i];
         }
         return aux;
@@ -15,17 +23,21 @@ function cra() {
     function somalista(lst) {
         // array -> array
         // retorna a soma dos elementos de uma array
-        var aux = lst;
+        var aux = [];
+        for (var i = 0; i < lst.length; i++) {
+            aux[i] = lst[i];
+        }
         while (aux.length > 1) {
-            aux[0] = lst[0] + lst[lst.length - 1];
-            lst.pop();
+            aux[0] = aux[0] + aux[aux.length - 1];
+            aux.pop();
         }
         return aux[0];
     }
-    var aux = multiplicalista(nota, cargahoraria);
-    var somadetudo = somalista(aux);
+    var aux1 = multiplicalista(notas, cargahoraria);
+    var somadetudo = somalista(aux1);
     var totalhoras = somalista(cargahoraria);
     var total = somadetudo / totalhoras;
-    var reprovacao = 1 - (0.5 * (cargareprov / totalhoras));
-    return total * reprovacao;
+    var reprovacao = 1 - (0.5 * (somalista(cargareprov) / totalhoras));
+    var resp = total * reprovacao;
+    document.getElementById('resposta').innerHTML = "Seu CRA é " + resp;
 }
